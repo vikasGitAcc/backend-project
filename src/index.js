572df3d9090e06854a0989d51fcc16e82dfe5edf
vikasGitAcc <vikassindhad12345@gmail.com>
2025-08-21 23:31:19 +0530

    error handling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import dotenv from "dotenv"
 import dbConnect from "./db/dbConnect.js"
+import { app } from "./app.js"
 dotenv.config({path:"./.env"})
-dbConnect()
+const port = process.env.PORT
+dbConnect().then(()=>{
+    app.listen(port||4000,()=>{
+        console.log(`Server is running at PORT : http://localhost:${port}`)
+    })
+}).catch((err)=>console.log("MongoDB connection failed : ",err))
---
+++ src/utils/asyncHandler/asyncHandler.js
@@ -0,0 +1,14 @@
+
+
+const asyncHandler = (fun) => async(req,res,next) => {
+      try{
+         await fun(req,res,next)
+      }catch(err){
+          res.status(err.code || 500)
+          res.json({success:false,
+            message:err.message
+          })
+      }
+}
+
+export {asyncHandler}
